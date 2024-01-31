7cd96a97e604cd606d30d6756217a4cbdf4b9af8
yahuarlocro <yahuarlocro@tutanota.com>
2024-01-31 17:51:12 +0100

    create exercise 2-7

---
+++ 02-Operating-Systems-and-Linux-Basics/07-node-app-check-status.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#title           :07-node-app-check-status.sh
+#description     :This script installs nodejs, and npm. Also downloads an 
+#                :application. Then it sets up the needed environment, installs
+#                :needed packages, and runs the application.
+#                ;Checks aoo sucessful start and shows app running process
+#author          :yahuarlocro
+#date            :30-01-2024
+#version         :0.1
+#usage           :bash 07-node-app-check-status.sh
+#                :./07-node-app-check-status.sh
+#dependecies     :
+#==============================================================================
+
+set -e 
+
+sudo apt update
+
+sudo apt install nodejs
+
+
+echo "################################################"
+echo "################################################"
+echo "################################################"
+echo "Node is installed and version is the following:"
+echo $(node -v)
+echo "################################################"
+echo "################################################"
+echo "################################################"
+
+sudo apt install npm
+
+wget https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz
+
+tar zxvf bootcamp-node-envvars-project-1.0.0.tgz
+
+export APP_ENV=dev
+export DB_USER=myuser
+export DB_PWD=mysecret
+
+cd package
+
+npm install
+
+node server.js &
+
+
+
+
---
+++ 02-Operating-Systems-and-Linux-Basics/exe7-Bash-Script-Node-App-Check-Status.md
@@ -0,0 +1,4 @@
+# Task
+Extend the script to check after running the application that the application has successfully started and prints out the application's running process and the port where it's listening.
+
+# Solution
