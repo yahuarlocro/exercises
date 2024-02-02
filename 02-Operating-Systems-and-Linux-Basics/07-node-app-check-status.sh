083a2129a5606c69320dbd7964e2840082809569
yahuarlocro <yahuarlocro@tutanota.com>
2024-02-02 11:28:37 +0100

    debug and finish exercise 2-7

--- 02-Operating-Systems-and-Linux-Basics/07-node-app-check-status.sh
+++ 02-Operating-Systems-and-Linux-Basics/07-node-app-check-status.sh
@@ -12,11 +12,11 @@
 #dependecies     :
 #==============================================================================
 
-set -e 
+set -e
 
 sudo apt update
 
-sudo apt install nodejs
+sudo apt install -y nodejs
 
 
 echo "################################################"
@@ -28,7 +28,7 @@ echo "################################################"
 echo "################################################"
 echo "################################################"
 
-sudo apt install npm
+sudo apt install -y npm
 
 wget https://node-envvars-artifact.s3.eu-west-2.amazonaws.com/bootcamp-node-envvars-project-1.0.0.tgz
 
@@ -42,7 +42,32 @@ cd package
 
 npm install
 
+npm audit fix --force
+
 node server.js &
+
+sleep 3
+
+PSID=$(pidof node)
+
+echo "${PSID}"
+
+PORT=$(ss -tulpn | grep "${PSID}" | awk -F' ' '{printf $5}')
+# PORT=$(ss -tulpn | awk -v psid="${PSID}"  '$0 ~ psid {print $5}')
+echo "${PORT}"
+
+
+if [[ -n ${PORT} ]]; then
+    echo "################################################"
+    echo "################################################"
+    echo "################################################"
+    echo "app is running on port"
+    echo ${PORT}
+    echo "################################################"
+    echo "################################################"
+    echo "################################################"
+
+fi
 
 
 
